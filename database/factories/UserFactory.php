da88f7d46d6ab629275274d119ef015098e03aea
Amitesh <amitesh@Admins-MacBook-Pro.local>
2020-07-26 05:08:48 +0530

    Bug fixed

--- database/factories/UserFactory.php
+++ database/factories/UserFactory.php
@@ -1,6 +1,7 @@
 <?php
 
 use Faker\Generator as Faker;
+use Illuminate\Support\Str;
 
 /*
 |--------------------------------------------------------------------------
@@ -18,7 +19,7 @@ $factory->define(App\Model\User::class, function (Faker $faker) {
         'name' => $faker->name,
         'email' => $faker->unique()->safeEmail,
         'password' => '$2y$10$TKh8H1.PfQx37YgCzwiKb.KjNyWgaHb9cbcoQgdIVFlYg7B77UdFm', // secret
-        'remember_token' => str_random(10),
-        'api_token' => str_random(60),
+        'remember_token' => Str::random(10),
+        'api_token' => Str::random(10)
     ];
 });
